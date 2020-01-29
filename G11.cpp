bd9be81a666b3e6604b5695dae6188097d84df4a
le3na <59690384+le3na@users.noreply.github.com>
2020-01-29 09:17:22 +0600

    my first code in c++

---
+++ G11.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pf printf
+#define sc scanf
+
+int main()
+{
+    printf("hello!!! This is LeeNa!!\n");
+    cout<<"hello!!! This is LeeNa !!"<<endl;
+}
